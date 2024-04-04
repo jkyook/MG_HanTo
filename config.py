c8a68a1d07df0c27e395b67677192f1e6c022fd9
jinkwan.yook@gmail.com <jinkwani79>
2024-04-04 09:06:43 +0900

    ver 1.2 memory clear

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 # API 인증 정보
 # api_key = "your_api_key"
 # secret_key = "your_secret_key"
-if 1 == 0:  # real
+if 1 == 1:  # real
     api_key = "PSpRVsKSNYjdOTmvcrPN0C0MyuqEZBaey2AC"
     secret_key = "KyTMYmD49Rbh+/DhtKYUuSRv6agjM9zxXs9IIHx9vz4UiCurqbpEPoawVFKNrx3DryhrLjxDy/vFbe/4acttdIU5hz6thCiPgeBLCEGpQcXvluQQWRNJg77ztOUPcPpqg3gVS+LxGOaOF9sB/n19fJmhf+O2cht6swH5Iz4aHUJZsZ0nrZM="
 
-if 1 == 1:  # demo
+if 1 == 0:  # demo
     api_key = "PSMID6MolzScnX0scR9WB7gZUK3cxrua4FwF"
     secret_key = "rTk4mvvNOEnF1iW6KV1/wCYR/ONhS1GjxktQN1YVC7YcguxMKWnin0x1XMfp8ansUwaNAo5a5mDPN+yNwgCc9HUWz5gaTyZWwB4VOCnXoXVjUfmkRzC3DEiyxL34lpPTz3woB7RJbKFKLHmxX7Rd3Iczla0p6y1Fst2TqT+52bN+Lmu1Z3s="
 
@@ -17,6 +17,7 @@ telegram_token = "5030631557:AAGFTf-C0XDWCViU3pOtLea5qSdiNSxDL7g"
 chat_id = "322233222"
 
 # 계좌 및 종목 정보
-account = "60025978"
+account = "64154012" # 실전
+# account = "60025978" # 모의
 code = "105V04"
 qty = 1  # 주문 수량
